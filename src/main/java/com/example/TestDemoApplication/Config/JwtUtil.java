c6420bea13f008189e10ae879454a7f026947d29
esandipanar <esandipankar@gmail.com>
2025-11-28 17:11:19 +0530

    completed

---
+++ src/main/java/com/example/TestDemoApplication/Config/JwtUtil.java
@@ -0,0 +1,31 @@
+package com.example.TestDemoApplication.Config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "dg922297";
+
+
+    public static String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 day
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public static String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+
+}
