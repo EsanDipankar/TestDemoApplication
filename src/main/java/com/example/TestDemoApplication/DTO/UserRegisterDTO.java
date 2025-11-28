3ba2cfd40a13a72a50486cb177f1c3f49978f771
esandipanar <esandipankar@gmail.com>
2025-11-28 11:20:54 +0530

    Done Till 11AM

---
+++ src/main/java/com/example/TestDemoApplication/DTO/UserRegisterDTO.java
@@ -0,0 +1,40 @@
+package com.example.TestDemoApplication.DTO;
+
+public class UserRegisterDTO {
+    private String userId;
+    private String usName;
+    private String password;
+    private String role;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsName() {
+        return usName;
+    }
+
+    public void setUsName(String usName) {
+        this.usName = usName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
